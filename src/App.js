49e33bbdd732caa1245d7577ac5f4e6245165686
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-26 14:04:38 -0500

    finished lecture

--- src/App.js
+++ src/App.js
@@ -2,20 +2,72 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { connect } from 'react-redux';
+
+// Allowing us to access redux state on props
+const mapReduxStateToProps = ({counterReducer, nameChanger, colorChanger}) => ({
+  counterReducer,
+  nameChanger,
+  colorChanger
+  // Is that all? Yes, that's it!
+});
+
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {name: '', color: ''}
+  }
+
+  changeName = (event) => {
+    this.setState({name: event.target.value});
+  }
+
+  submitNameChange = (event) => {
+    event.preventDefault();
+    this.props.dispatch({type: 'NAME', payload: this.state.name});
+    this.state.name = '';
+  }
+
+  changeColor = (event) => {
+    this.setState({color: event.target.value});
+  }
+
+  submitColorChange = (event) => {
+    event.preventDefault();
+    this.props.dispatch({type: 'COLOR', payload: this.state.color});
+    this.state.color = '';
+  }
+
+  deleteAllColors = () => {
+    this.props.dispatch({type: 'DELETE_COLORS'});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={() => {this.props.dispatch({type: 'ADD'})}}>Add</button>
+        <button onClick={() => {this.props.dispatch({type: 'SUBTRACT'})}}>Subtract</button>
+        <h1>{this.props.counterReducer}</h1>
+        <form onSubmit={this.submitNameChange}>
+          <input type="text" placeholder="name" onChange={this.changeName} value={this.state.name}/>
+          <input type="submit" value="Submit" />
+        </form>
+        <h1>{this.props.nameChanger.name}</h1>
+        <form onSubmit={this.submitColorChange}>
+          <input type="text" placeholder="background color" onChange={this.changeColor} value={this.state.color}/>
+          <input type="submit" value="Submit" />
+        </form>
+        <button onClick={this.deleteAllColors}>Delete All Colors</button>
+        <ul>
+          {this.props.colorChanger.map(color => <li>{color}</li>)}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+// Calling connect without passing anything in allows dispatch
+// To get information out, we need to pass mapReduxStateToProps to connect
+export default connect(mapReduxStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,49 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Must import createStore to begin redux store build
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger'; // Makes development easier
+
+const counterReducer = (state = 0, action) => {
+    // Filter down 
+    if(action.type === 'ADD'){
+    console.log('Hey, I\'m a reducer!', action, state);
+        return state + 1;
+    } else if(action.type === 'SUBTRACT'){
+        return state - 1;
+    }
+    return state;
+}
+
+const nameChanger = (state, action) => {
+    console.log('Second to the party, last to leave!');
+    console.log(action, state);
+    if(action.type === 'NAME'){
+        return {name: action.payload};
+    }
+    return {name: "Nope"};
+}
+
+const colorChanger = (state = [], action) => {
+    if(action.type === 'COLOR'){
+        return [...state, action.payload];
+    } else if(action.type === 'DELETE_COLORS'){
+        return [];
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    // This is a reducer.
+    combineReducers({
+        counterReducer,
+        nameChanger,
+        colorChanger
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
